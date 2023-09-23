beb26a6eafe904b82fb84c96455dade72f673358
IsaiahGoodridge <isaiahgoodridge@hotmail.com>
2023-09-23 15:28:29 -0400

    Layout
    Create Header, Nav, Main, and Footer components. Export components as default. Import components to App.js. Add components to App function in App.js.

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+        <header>
+          <img src="/assets/Logo.png" alt="Little Lemon Logo" className='Logo' />
+
+        </header>
+    </>
+  )
+}
+
+export default Header;
---
+++ src/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <>
+        <nav>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/products">Products</a></li>
+                <li><a href="/about">About Us</a></li>
+                <li><a href="/contact">Contact</a></li>
+            </ul>
+        </nav>
+    </>
+  )
+}
+
+export default Nav;
