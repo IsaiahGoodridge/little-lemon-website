53f48e27d84ad8a0f7f81bf8631af0b1e4f5b3f5
IsaiahGoodridge <isaiahgoodridge@hotmail.com>
2023-09-23 21:14:15 -0400

    Footer copyright
    Update footer copyright

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <>
-        <footer className='Footer'>Footer</footer>
+        <footer className='Footer'>Copyright &#169; 2023 Little Lemon. All Rights Reserved.</footer>
     </>
   )
 }
