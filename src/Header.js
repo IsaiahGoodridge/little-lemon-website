0860bdf52ea81a5bc1b14653341398e0410f5f77
IsaiahGoodridge <isaiahgoodridge@hotmail.com>
2023-09-23 21:07:59 -0400

    Styling for Main
    Complete initial styling for main.  Fix footer to bottom of page.

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,13 @@
-import React from 'react'
+import React from 'react';
 import Nav from './Nav';
+import Logo from './assets/Logo.svg';
 
 const Header = () => {
   return (
     <>
         <header className='Header'>
           <div className='Logo'>
-            <img src="/assets/Logo.png" alt="Little Lemon Logo" className='Logo-pic' />
+            <img src={Logo} alt="Little Lemon Logo" className='Logo-pic' />
           </div>
           <Nav/>
 
--- src/Main.js
+++ src/Main.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import greeksalad from './assets/greek salad.jpg';
+import bruchetta from './assets/bruchetta.svg'
+import lemondessert from  './assets/lemon dessert.jpg'
 
 const Main = () => {
   return (
@@ -14,7 +17,7 @@ const Main = () => {
               </button>
             </section>
           </article>
-          <article className='Article-container Specials-container'>
+          <article className='Article-container Specials-article-container'>
             <section className='This-weeks-specials-container'>
               <h1 className='Specials-title'>This weeks specials!</h1>
               <button className='Btn Menu-btn'>
@@ -23,33 +26,48 @@ const Main = () => {
             </section>
             <section className='Specials-container'>
               <div className='Specials'>
-                <h5 className='Specials-name'>Special One</h5>
-                <p className='Specials-price'>Price</p>
-                <div className='Specials-details-container'>
-                  <p className='Specials-details-text'>Text</p>
-                </div>
-                <div className='Specials-order-link-container'>
-                  <p className='Specials-order-link'>Order a delivery</p>
-                </div>
-              </div>
-              <div className='Specials'>
-                <h5 className='Specials-name'>Special Two</h5>
-                <p className='Specials-price'>Price</p>
-                <div className='Specials-details-container'>
-                  <p className='Specials-details-text'>Text</p>
-                </div>
-                <div className='Specials-order-link-container'>
-                  <p className='Specials-order-link'>Order a delivery</p>
+                <img src={greeksalad} alt='Image of greek salad' className='Food-image'/>
+                <div className='Specials-padding'>
+                  <div className='Specials-name-and-price'>
+                    <h5 className='Specials-name'>Greek Salad</h5>
+                    <h5 className='Specials-price'>$12.99</h5>
+                  </div>
+                  <div className='Specials-details-container'>
+                    <p className='Specials-details-text'>The famous greek salad of crispy lettuce, peppers, olives, and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons</p>
+                  </div>
+                  <div className='Specials-order-link-container'>
+                    <p className='Specials-order-link'>Order a delivery</p>
+                  </div>
                 </div>
               </div>
               <div className='Specials'>
-                <h5 className='Specials-name'>Special Three</h5>
-                <p className='Specials-price'>Price</p>
-                <div className='Specials-details-container'>
-                  <p className='Specials-details-text'>Text</p>
+                <img src={bruchetta} alt='Image of bruchetta' className='Food-image'/>
+                <div className='Specials-padding'>
+                  <div className='Specials-name-and-price'>
+                    <h5 className='Specials-name'>Bruchetta</h5>
+                    <h5 className='Specials-price'>$5.99</h5>
+                  </div>
+                  <div className='Specials-details-container'>
+                    <p className='Specials-details-text'>Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with sald and olive oil</p>
+                  </div>
+                  <div className='Specials-order-link-container'>
+                    <p className='Specials-order-link'>Order a delivery</p>
+                  </div>
                 </div>
-                <div className='Specials-order-link-container'>
-                  <p className='Specials-order-link'>Order a delivery</p>
+              </div>
+              <div className='Specials'>
+                <img src={lemondessert} alt='Image of lemon dessert' className='Food-image'/>
+                <div className='Specials-padding'>
+                  <div className='Specials-name-and-price'>
+                    <h5 className='Specials-name'>Lemon Dessert</h5>
+                    <h5 className='Specials-price'>$5.00</h5>
+                  </div>
+                  <div className='Specials-details-container'>
+                    <p className='Specials-details-text'>This comes straight from Grandma's recipe book, every last ingredient is as authentic as can be imagined.</p>
+                  </div>
+                  <div className='Specials-order-link-container'>
+                    <p className='Specials-order-link'>Order a delivery</p>
+                  </div>
                 </div>
               </div>
             </section>
