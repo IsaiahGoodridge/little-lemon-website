d44aff950cde3e6a45a084c4e0e6b3a9d5a3e944
IsaiahGoodridge <isaiahgoodridge@hotmail.com>
2023-09-28 16:31:28 -0400

    Updated Nav.js
    Update Nav.js, changing <li> to Links and Routes, attempting to create a SPA. Clicking on the links are not replacing current content. Will move on and fix later.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
+import {Route, Routes} from 'react-router-dom'
 
 function App() {
   return (
